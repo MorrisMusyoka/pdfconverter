ae6b7a45d9758a4777178e45898774e18e23b78a
MorrisMusyoka <morrismusyoka19@gmail.com>
2022-12-20 12:23:11 +0300

    initial commit

---
+++ convert/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from django.views import View
+from django.core.files.storage import FileSystemStorage
+from .pypdf import Convert
+# Create your views here.
+
+
+class Upload(View):
+    def get(self, request):
+        context = {
+            'name': "",
+            'convert': {
+                'ready': False,
+            },
+            'download': {
+                'isDone': False,
+                'link': "\\"
+            }
+        }
+        return render(request, "index.html", context)
+
+    def post(self, request):
+        uploadedfile = request.FILES['uploadFile']
+        fs = FileSystemStorage()
+        fs.save(uploadedfile.name, uploadedfile)
+
+        context = {
+            'name': uploadedfile.name,
+            'convert': {
+                'ready': True,
+                'type': 'None',
+            },
+            'download': {
+                'ready': False,
+                'link': "\\"
+            }
+        }
+        return render(request, "index.html", context)
+
+
+class ConvertFile(View):
+    def post(self, request):
+        convertFormat = request.POST['convertFormat']
+        name = request.POST['name']
+        Convert(name[:-4], convertFormat)
+
+        context = {
+            'name': name,
+            'convert': {
+                'ready': True,
+                'type': convertFormat,
+            },
+            'download': {
+                'ready': True,
+                'link':  name[:-4]+convertFormat,
+            }
+        }
+        return render(request, "index.html", context)
---
+++ pdfconverter/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for pdfconverter project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import shutil
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pdfconverter.settings')
+
+application = get_wsgi_application()
+
+shutil.rmtree('convert\\temp\\word\\')
+os.mkdir('convert\\temp\\word\\')
